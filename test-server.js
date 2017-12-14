7d43c46e1d7f4647d0a96cf2616498b6eabe1de6
moonrailgun <moonrailgun@gmail.com>
2017-12-14 15:32:55 +0800

    add unit test suit

--- test-server.js
+++ test-server.js
@@ -8,37 +8,41 @@ const Boom = require('boom')
 const _ = require('koa-route')
 const logger = require('./index')
 
-const app = new Koa()
-app.use(logger())
+module.exports = function(opts) {
+  const app = new Koa()
+  app.use(logger(opts))
 
-app.use(_.get('/200', function (ctx) {
-  ctx.body = 'hello world'
-}))
+  app.use(_.get('/200', function (ctx) {
+    ctx.body = 'hello world'
+  }))
 
-app.use(_.get('/301', function (ctx) {
-  ctx.status = 301
-}))
+  app.use(_.get('/301', function (ctx) {
+    ctx.status = 301
+  }))
 
-app.use(_.get('/304', function (ctx) {
-  ctx.status = 304
-}))
+  app.use(_.get('/304', function (ctx) {
+    ctx.status = 304
+  }))
 
-app.use(_.get('/404', function (ctx) {
-  ctx.status = 404
-  ctx.body = 'not found'
-}))
+  app.use(_.get('/404', function (ctx) {
+    ctx.status = 404
+    ctx.body = 'not found'
+  }))
 
-app.use(_.get('/500', function (ctx) {
-  ctx.status = 500
-  ctx.body = 'server error'
-}))
+  app.use(_.get('/500', function (ctx) {
+    ctx.status = 500
+    ctx.body = 'server error'
+  }))
 
-app.use(_.get('/500-boom', function (ctx) {
-  ctx.throw(Boom.badImplementation('terrible implementation'))
-}))
+  app.use(_.get('/500-boom', function (ctx) {
+    ctx.throw(Boom.badImplementation('terrible implementation'))
+  }))
 
-app.use(_.get('/error', function (ctx) {
-  throw new Error('oh no')
-}))
+  app.use(_.get('/error', function (ctx) {
+    throw new Error('oh no')
+  }))
 
-module.exports = app
+  return app
+}
+
+// module.exports = app
