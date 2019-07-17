225b98e5842648851e90ee3102605545e5adb0b3
haoxin <haoxinst@gmail.com>
2019-07-17 10:13:11 +0800

    lint

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function dev (opts) {
     }
 
     return function printFunc (...args) {
-      let str = util.format(...args)
+      const str = util.format(...args)
       if (transporter) {
         transporter(str, args)
       } else {
@@ -60,8 +60,8 @@ function dev (opts) {
     print('  ' + chalk.gray('<--') +
       ' ' + chalk.bold('%s') +
       ' ' + chalk.gray('%s'),
-        ctx.method,
-        ctx.originalUrl)
+    ctx.method,
+    ctx.originalUrl)
 
     try {
       await next()
@@ -113,6 +113,7 @@ function log (print, ctx, start, len, err, event) {
 
   // set the color of the status code;
   const s = status / 100 | 0
+  // eslint-disable-next-line
   const color = colorCodes.hasOwnProperty(s) ? colorCodes[s] : colorCodes[0]
 
   // get the human readable response length
@@ -127,7 +128,7 @@ function log (print, ctx, start, len, err, event) {
 
   const upstream = err ? chalk.red('xxx')
     : event === 'close' ? chalk.yellow('-x-')
-    : chalk.gray('-->')
+      : chalk.gray('-->')
 
   print('  ' + upstream +
     ' ' + chalk.bold('%s') +
@@ -135,11 +136,11 @@ function log (print, ctx, start, len, err, event) {
     ' ' + chalk[color]('%s') +
     ' ' + chalk.gray('%s') +
     ' ' + chalk.gray('%s'),
-      ctx.method,
-      ctx.originalUrl,
-      status,
-      time(start),
-      length)
+  ctx.method,
+  ctx.originalUrl,
+  status,
+  time(start),
+  length)
 }
 
 /**
